a4204a22d02b74c4203a22e6c4789849cb041094
Rico Trebeljahr <Nrin3141@users.noreply.github.com>
2020-05-08 09:58:38 +0200

    Add some random examples

--- first-clojure-app/src/first_clojure_app/core.clj
+++ first-clojure-app/src/first_clojure_app/core.clj
@@ -1,7 +1,26 @@
 (ns first-clojure-app.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World! You know - I am a little teapot!"))
+(defn greeting [greet]
+  (str greet " World! You know - I am a little teapot!"))
+
+(greeting "Hello")
+
+1
+"a string"
+["a" "vector" "of" "strings"]
+
+(+ 1 2 3)
+
+(str "A" "B" "C"
+     (if true
+       (do (str "D" " AND E!")))) j
+
+(when true (str "Success!"
+                " Something Else!"))
+
+(if false
+  "Hello there"
+  "Well...")
+
+(str "Hello" " World")
