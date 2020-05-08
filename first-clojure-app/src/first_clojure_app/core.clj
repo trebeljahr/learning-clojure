4efb6e9e5a4cfa3c02e6f95c5b874a9a8b88b523
Rico Trebeljahr <Nrin3141@users.noreply.github.com>
2020-05-08 10:30:23 +0200

    Add some language do-hickeys

--- first-clojure-app/src/first_clojure_app/core.clj
+++ first-clojure-app/src/first_clojure_app/core.clj
@@ -19,8 +19,65 @@
 (when true (str "Success!"
                 " Something Else!"))
 
+// Conditionals 
+(if true 
+  "Hello there"
+  "Well...")
+
 (if false
   "Hello there"
   "Well...")
 
-(str "Hello" " World")
+// All Nil Things
+(nil? 1)
+(nil? (str "Hello there"))
+(nil? 0)
+(nil? -1)
+(nil? false)
+(nil? nil)
+
+(if nil 
+  "Nil is true value"
+  "Nil is a lie!"
+  )
+
+// Logical operators!
+(= 1 -1)
+(= 1 1)
+(= nil 2)
+(= nil nil)
+
+// or returns the first true value
+(or false false false false false "Hello there")
+(or false true)
+
+// and returns the first false value or the last true value
+(and false false)
+(and false true)
+(and true true "Hello!")
+
+// defining variables with def 
+
+// There are strings...
+(def my_name "Rico")
+(str "Hello " my_name)
+
+// and numbers...
+(def my_age 21)
+(str "I am " my_age " years old!")
+
+// and vectors...
+(def my_greetings ["Hi" "Hello" "Howdy" "How are you"])
+
+// and map-literals...
+(def my_person {:name "Rico" :age 21 :gender "male" :level "over 900" })
+(str "My level is " (get my_person :level))
+(str "Hello my name is " (get my_person :name) "." 
+  " I am " (get my_person :age) " years old and " 
+  "my level is " (get my_person :level))
+
+// and hash maps... 
+(def my_level (hash-map :a " is over " :b 9000))
+(str "My level" (get my_level :a) (get my_level :b))
+
+
