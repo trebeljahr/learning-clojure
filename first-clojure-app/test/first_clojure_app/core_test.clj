8cfa65f07191f47d060289d793df0e5aa7756a2f
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-05-31 21:36:40 +0200

    Move clj files out of "project" structure

--- first-clojure-app/test/first_clojure_app/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns first-clojure-app.core-test
-  (:require [clojure.test :refer :all]
-            [first-clojure-app.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
